e2ce679ce5a2bbf9db27cee8e0198840e5c33083
Arnie97 <arnie97@gmail.com>
2018-04-23 13:24:42 +0800

    Read the OAuth token from environment variables.

--- scan.py
+++ scan.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 
+import os
 import json
 import requests
 from typing import Tuple
 
 API = 'https://api.github.com'
+TOKEN = {'Authorization': 'token ' + os.environ.get('GITHUB_TOKEN', '')}
 
 
 def test_login(*auth: Tuple[str, str]) -> bool:
     'Try to login with the username and password.'
-    r = requests.get(API + '/user', auth=auth)
+    r = requests.get(API, auth=auth)
     return r.status_code == 200
 
 
 def search_code(keywords: str) -> dict:
     'Search across all the public repositories.'
-    r = requests.get(API + '/search/code', dict(q=keywords))
+    r = requests.get(API + '/search/code', dict(q=keywords), headers=TOKEN)
     return json.loads(r.text)
